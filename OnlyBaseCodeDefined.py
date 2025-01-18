f3b91b4eb61e964335064b8e817821272cea74ad
parris3142 <parris3142@hotmail.com>
2025-01-19 06:08:55 +0800

    Add specific exception type or case where alternative schemes are requested but do not exist

---
+++ OnlyBaseCodeDefined.py
@@ -0,0 +1,8 @@
+class OnlyBaseCodeDefined(LookupError):
+
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f'OnlyBaseCodeDefined: {self.message}'
